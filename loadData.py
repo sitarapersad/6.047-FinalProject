e8b319cac0f8964a7693ca14a8a2b03a274033ad
Joseph Lin <joelin@mit.edu>
2016-12-07 21:00:18 -0500

    better merging

--- loadData.py
+++ loadData.py
@@ -40,17 +40,6 @@ diseases['bip'] = {'sample_size': (6990, 4820), 'filename': 'pgc.cross.BIP11.201
 diseases['mdd'] = {'sample_size': (9227, 7383), 'filename': 'pgc.cross.MDD9.2013-05.txt'}
 diseases['scz'] = {'sample_size': (9379, 7736), 'filename': 'pgc.cross.SCZ17.2013-05.txt'}
 
-
-def get_genetic_corr(disease1_file, disease2_file):
-    '''Runs mungestat and ldsc on two diseases to estimate the genetic correlation'''
-    
-    
-sample_sizes = {'aut': (4788 + 161, 4788 + 526),
-                'add': (1947 + 840, 1947 + 688),
-                'bip': (6990, 4820),
-                'mdd': (9227, 7383),
-                'scz': (9379, 7736)}
-                
 def munge(disease1, disease2):
     ''' Runs mungestat on two diseases in preparation for ldsc'''
 
