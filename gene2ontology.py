d2266432b2c53558355119dac8b60af8ef552532
Joseph Lin <joelin@mit.edu>
2016-12-10 21:56:19 -0500

    Finished getting annotations from genes of interest

--- gene2ontology.py
+++ gene2ontology.py
@@ -1,47 +1,69 @@
 import MySQLdb
+import pandas as pd
 
-db = MySQLdb.connect(host="mysql-amigo.ebi.ac.uk",    # your host, usually localhost
-                     user="go_select",         # your username
-                     passwd="amigo",  # your password
-                     db="go_latest",
-                     port=4085)        # name of the data base
+def get_conn():
+    db = MySQLdb.connect(host="mysql-amigo.ebi.ac.uk",    # your host, usually localhost
+                         user="go_select",         # your username
+                         passwd="amigo",  # your password
+                         db="go_latest",
+                         port=4085)        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+    return db, cur
+
 
 # Use all the SQL you like
-sql="""
-SELECT
- term.name AS superterm_name,
- term.acc AS superterm_acc,
- term.term_type AS superterm_type,
- association.*,
- gene_product.symbol AS gp_symbol,
- gene_product.symbol AS gp_full_name,
- dbxref.xref_dbname AS gp_dbname,
- dbxref.xref_key AS gp_acc,
- species.genus,
- species.species,
- species.ncbi_taxa_id,
- species.common_name
-FROM term
- INNER JOIN graph_path ON (term.id=graph_path.term1_id)
- INNER JOIN association ON (graph_path.term2_id=association.term_id)
- INNER JOIN gene_product ON (association.gene_product_id=gene_product.id)
- INNER JOIN species ON (gene_product.species_id=species.id)
- INNER JOIN dbxref ON (gene_product.dbxref_id=dbxref.id)
-WHERE
- species.genus = 'Homo'
- AND
- species.species = 'sapiens'
- AND
- gene_product.symbol = 'SLC35F3';
-"""
+def get_annotations(genes):
+    db, cur = get_conn()
+    format_strings = ','.join(['%s'] * len(genes))
 
-cur.execute(sql)
+    sql="""
+        SELECT DISTINCT
+            gene_product.symbol,
+            term.acc,
+            term.name,
+            term.term_type,
+            gene_product.full_name
 
-for row in cur.fetchall():
-    print row
+        FROM
+            gene_product
+            INNER JOIN dbxref ON (gene_product.dbxref_id=dbxref.id)
+            INNER JOIN species ON (gene_product.species_id=species.id)
+            INNER JOIN association ON (gene_product.id=association.gene_product_id)
+            INNER JOIN evidence ON (association.id=evidence.association_id)
+            INNER JOIN term ON (association.term_id=term.id)
+        WHERE
+            gene_product.symbol IN (%s)
+            AND species.genus = 'homo'
+            AND species.species = 'sapiens'
+        ORDER BY gene_product.symbol, term.term_type, term.acc
+        ;
+        """ % format_strings
 
-db.close()
+    df = pd.read_sql(sql, params=tuple(genes), con=db)
+    db.close()
+    return df
+
+
+def get_genes(disease1, disease2):
+    f = open('genes_of_interest/unique_genes_'+disease1+'_'+disease2+'.txt', 'r')
+    genes = []
+    for line in f:
+        genes.append(line[:-1])  # remove \n
+
+    return genes
+
+def get_annotations_from_disease(disease1, disease2):
+    df = get_annotations(get_genes(disease1, disease2))
+    df.to_csv('annotations/annot_'+disease1+'_'+disease2+'.csv')
+    return df
+
+
+if __name__ == '__main__':
+    diseases = ['aut', 'add', 'bip', 'mdd', 'scz']
+    for i in xrange(len(diseases) - 1):
+        for j in xrange(i + 1, len(diseases)):
+            print diseases[i], diseases[j]
+            get_annotations_from_disease(diseases[i], diseases[j])
